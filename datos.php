c7d8a36223ca58479f2dfd2a1795f5667b0ca048
msdaniel358 <usu32@A32O09.novaulas.stucom.com>
2016-11-17 17:02:30 +0100

    recoge datos

---
+++ datos.php
@@ -0,0 +1,12 @@
+<?php
+$nombre= $_POST["nombre"];
+$edad= $_POST["edad"];
+$ciudad=$_POST["ciudad"];
+$sexo= $_POST["sexo"];
+
+echo "<p>Datos del usuario registrado en el formulario</p>";
+echo "<p>Nombre: $nombre Edad: $edad CIudad:$ciudad sexo:$sexo</p>";
+
+
+
+?>
